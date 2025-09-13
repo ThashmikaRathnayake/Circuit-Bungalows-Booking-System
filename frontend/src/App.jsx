a5341bbd5fd416c43307cdbd0159deace4d910c1
ThashmikaRathnayake <thashrath7@gmail.com>
2025-09-13 18:27:31 +0530

    feat: add protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import InfoPage from "./components/InfoPage";
 import BookingPage from "./pages/BookingPage";
 import AdminDashboard from "./pages/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
@@ -15,18 +16,30 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
+
+  //Protected Routes
   {
+    element: <ProtectedRoute allowedRoles={['user']} />,
+    children: [
+      {
     path: "/infoPage",
     element: <InfoPage />
   },
   {
     path: "/booking/:circuit", 
     element: <BookingPage />
+  } ]
   },
+  
   {
-    path: "/admin",
-    element: <AdminDashboard/>
+    element: <ProtectedRoute allowedRoles={['admin']} />,
+    children: [
+      {
+        path: "/admin",
+        element: <AdminDashboard/>
+      } ]
   }
+  
   
 ]);
 
