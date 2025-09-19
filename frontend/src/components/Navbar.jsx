a4d8dee21a1346a3a52956ad8866ea83a7ca1489
NethmiPramoddya <nethmik2001@gmail.com>
2025-09-19 18:04:32 +0530

    admin Routes minor bug fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/admin/addLocation" className="hover:text-gray-300 transition-colors duration-300">
+                            <Link to="/admin/BungalowTable" className="hover:text-gray-300 transition-colors duration-300">
                                 Update
                             </Link>
                         </li>
@@ -116,7 +116,7 @@ const Navbar = () => {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/admin/bungalowTable" className="hover:text-gray-300 transition-colors duration-300">
+                                    <Link to="/admin/BungalowTable" className="hover:text-gray-300 transition-colors duration-300">
                                         Update
                                     </Link>
                                 </li>
