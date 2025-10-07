3a67d22f92861f2f02bfa5f7237008f9a297ff8f
ThashmikaRathnayake <thashrath7@gmail.com>
2025-10-08 00:26:53 +0530

    minor fix

--- frontend/src/pages/BookingForm.jsx
+++ frontend/src/pages/BookingForm.jsx
@@ -126,7 +126,7 @@ export default function BookingForm() {
   const addRow = () => {
     setFormData({
       ...formData,
-      substitutes: [...formData.substitutes, { name: "", nic: "", designation: "", department: "" }],
+      substitutes: [...formData.substitutes, { name: "", nic: "", relationship: "" }],
     });
   };
 
